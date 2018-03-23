e99c76bc93d28180fab63d5076cc490fb664e566
Daedgomez <daedgomez@gmail.com>
2018-03-23 13:06:39 -0600

    [Add] Population and generation functions ready

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 from tec.ic.ia.pc1.g0x import generar_muestra_pais, generar_muestra_provincia
-generar_muestra_pais(1)
-
+print(generar_muestra_pais(5))
+print(generar_muestra_provincia(5,"CARTAGO"))
