be6e869b56d3e737210b212532b9e093a89ce605
Brayan <brayan.f.a07@hotmail.com>
2018-03-23 10:46:20 -0600

    Agregación del archivo test_g0x.py

--- tec/ic/ia/pc1/g0x.py
+++ tec/ic/ia/pc1/g0x.py
@@ -1,14 +1,26 @@
 import csv, operator
+
 csvURL = 'datos.csv'
-with open(csvURL) as csvarchivo:
-    entrada = csv.reader(csvarchivo)
-    for reg in entrada:
-        print(reg)
+
+def open_file_CSV():
+	with open(csvURL) as csvarchivo:
+		entrada = csv.reader(csvarchivo)
+		for reg in entrada:
+			print(reg)
+
 
 def generar_muestra_pais(n):
-	print("func1")
+
+	return 5
+	#print("func1")
 
 def generar_muestra_provincia(n,nombre_provincia):
-	print("func2")
+	return 4
+	#print("func2")
+
+
+def inc(n):
+	n = n +1
+	return n
 
 
