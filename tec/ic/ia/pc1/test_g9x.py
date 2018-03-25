4927bd733a417394b9418f6f0d65a8bf476bb0c9
Brayan <brayan.f.a07@hotmail.com>
2018-03-25 17:04:03 -0600

    Cambios en el orden de los archivos dentro de las carpetas

--- tec/ic/ia/pc1/test_g9x.py
+++ tec/ic/ia/pc1/test_g9x.py
@@ -1,5 +1,5 @@
 
-from g0x import *
+from g9x import *
 
 def test_generar_poblacion():
 	assert generar_poblacion() == 0
@@ -10,3 +10,10 @@ def test_generar_muestra_pais():
 def test_generar_muestra_provincia():
 	assert len(generar_muestra_provincia(5,"LIMON")) > 2
 
+def test_generar_random():
+	assert generar_random(100) <= 100 and generar_random(100)>=0
+
+def test_generar_estado_vivienda():
+	assert generar_estado_vivienda(50) == "Vivienda en buen estado"
+
+def 
