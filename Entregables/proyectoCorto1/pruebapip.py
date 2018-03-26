b67e4cbcf8239426ee6c66be410ffa037f153d16
Daedgomez <daedgomez@gmail.com>
2018-03-25 21:46:32 -0600

    [Add] File from lib

---
+++ Entregables/proyectoCorto1/pruebapip.py
@@ -0,0 +1,3 @@
+from tec.ic.ia.pc1.g09 import generar_muestra_pais, generar_muestra_provincia
+generar_muestra_pais(5)
+
--- Entregables/proyectoCorto1/tec/ic/ia/pc1/g09/__init__.py
+++ Entregables/proyectoCorto1/tec/ic/ia/pc1/g09/__init__.py
@@ -1,8 +1,11 @@
 import csv  # Para abrir el archivo csv y cargar los datos
 import random  # Para generar numeros aleatorios
-
+import os
+PARENT_DIR = os.path.dirname(os.path.dirname(__file__))
+csvURL = os.path.join(PARENT_DIR, 'g09', 'DatosTSE.csv')
+   
 # Variables globales
-csvURL = 'DatosTSE.csv'  # Direccion donde se encuentra el archivo csv a leer
+#csvURL = 'DatosTSE.csv'  # Direccion donde se encuentra el archivo csv a leer
 poblacion = []  # Lista donde se guardan todos los individuos creados
 
 
